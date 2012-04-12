c46bf795ae41fe6ee7180a109315af6ed2da56b6
Billy Mills <billymills@dhcp-1-148.rbh.rockynet.wireless.unca>
2012-04-12 15:04:30 -0400

    horizontal lines drawn

--- scripts/proj1p2.js
+++ scripts/proj1p2.js
@@ -9,14 +9,24 @@ function init() {
 }
 
 function drawGrid(myCanvas, myCtx) {
+//var and for loop to draw vertical lines
 var x = 120;
+for(i=0; i<4; i++) {
+	myCtx.beginPath();
+	myCtx.moveTo(x,0);
+	myCtx.lineTo(x,500);
+	myCtx.stroke();
+	x = x+120;
+}; //end for loop
 
-	for(i=0; i<4; i++) {
-		myCtx.beginPath();
-		myCtx.moveTo(x,0);
-		myCtx.lineTo(x,500);
-		myCtx.stroke();
-		x = x+120;
-	};
+//var and for loop to draw horizontal lines
+var y = 50;
+for(i=0; i<10; i++) {
+	myCtx.beginPath();
+	myCtx.moveTo(0,y);
+	myCtx.lineTo(600,y);
+	myCtx.stroke();
+	y = y + 50;
+}; //end for loop
 	
 }
